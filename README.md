52d07ecfead797963c4440ac9d4d58cdbebf699f
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-06-26 14:53:17 +0100

    Initial Upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Getting Started
+
+1. Clone the repository
+2. Run npm install or npm i
+3. Run nodemon app.js
---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const currentYear = new Date().getFullYear();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`); // Log message when the server starts
+});
